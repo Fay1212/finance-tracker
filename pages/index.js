c05b750a84cc247dd95d0eed1afe90907ce72142
Fay <chocofay1@gmail.com>
2026-07-15 09:51:18 +0700

    Add homepage

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello World from Vercel!</h1>
+      <p>This is my first Next.js page.</p>
+    </div>
+  );
+}
+
